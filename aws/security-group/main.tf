5e90d4012404bb8772d25087ae57fe8942dfb9cd
Serban Gilvitu <serbangilvitu@users.noreply.github.com>
2020-08-04 22:16:36 +0400

    aws/security-group: fixed self ingress, added description

--- aws/security-group/main.tf
+++ aws/security-group/main.tf
@@ -3,6 +3,20 @@ resource "aws_security_group" "security_group" {
   description = var.description
   vpc_id = var.vpc_id
 
+//  Ingress - Self
+  dynamic "ingress" {
+    for_each = var.ingress_from_self_list
+
+    content {
+      from_port = element(split(",", ingress.value), 0)
+      to_port = element(split(",", ingress.value), 1)
+      protocol = element(split(",", ingress.value), 2)
+      description = element(split(",", ingress.value), 3)
+      self = true
+    }
+
+  }
+
 //  Ingress - Source SG
   dynamic "ingress" {
     for_each = var.ingress_source_sg_list
@@ -12,6 +26,7 @@ resource "aws_security_group" "security_group" {
       to_port = element(split(",", ingress.value), 1)
       protocol = element(split(",", ingress.value), 2)
       security_groups = [ element(split(",", ingress.value), 3) ]
+      description = element(split(",", ingress.value), 4)
     }
 
   }
@@ -25,6 +40,7 @@ resource "aws_security_group" "security_group" {
       to_port = element(split(",", ingress.value), 1)
       protocol = element(split(",", ingress.value), 2)
       cidr_blocks = [ element(split(",", ingress.value), 3) ]
+      description = element(split(",", ingress.value), 4)
     }
 
   }
@@ -39,6 +55,7 @@ resource "aws_security_group" "security_group" {
       to_port = element(split(",", egress.value), 1)
       protocol = element(split(",", egress.value), 2)
       security_groups = [ element(split(",", egress.value), 3) ]
+      description = element(split(",", egress.value), 4)
     }
 
   }
@@ -52,6 +69,7 @@ resource "aws_security_group" "security_group" {
       to_port = element(split(",", egress.value), 1)
       protocol = element(split(",", egress.value), 2)
       cidr_blocks = [ element(split(",", egress.value), 3) ]
+      description = element(split(",", egress.value), 4)
     }
 
   }
@@ -59,17 +77,4 @@ resource "aws_security_group" "security_group" {
   tags = merge({
     Name = "${var.name_prefix}"
   }, var.additional_tags)
-}
-
-//  Ingress - Self
-resource "aws_security_group_rule" "ingress_self" {
-  for_each = toset(var.ingress_from_self_list)
-
-  from_port = element(split(",", each.value), 0)
-  to_port = element(split(",", each.value), 1)
-  protocol = element(split(",", each.value), 2)
-  description = element(split(",", each.value), 3)
-  security_group_id = aws_security_group.security_group.id
-  source_security_group_id = aws_security_group.security_group.id
-  type = "ingress"
 }
