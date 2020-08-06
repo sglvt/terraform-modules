2062797a569fd55d9708d823763a67d9345b2572
Serban Gilvitu <serbangilvitu@users.noreply.github.com>
2020-08-06 20:25:44 +0400

    aws/security-group: added support for IPv6 and prefix lists

--- aws/security-group/main.tf
+++ aws/security-group/main.tf
@@ -3,6 +3,20 @@ resource "aws_security_group" "security_group" {
   description = var.description
   vpc_id = var.vpc_id
 
+//  Ingress - CIDR IPv4
+  dynamic "ingress" {
+    for_each = var.ingress_cidr_list
+
+    content {
+      from_port = element(split(",", ingress.value), 0)
+      to_port = element(split(",", ingress.value), 1)
+      protocol = element(split(",", ingress.value), 2)
+      cidr_blocks = [ element(split(",", ingress.value), 3) ]
+      description = element(split(",", ingress.value), 4)
+    }
+
+  }
+
 //  Ingress - Self
   dynamic "ingress" {
     for_each = var.ingress_from_self_list
@@ -13,6 +27,35 @@ resource "aws_security_group" "security_group" {
       protocol = element(split(",", ingress.value), 2)
       description = element(split(",", ingress.value), 3)
       self = true
+    }
+
+  }
+
+//  Ingress - CIDR IPv6
+  dynamic "ingress" {
+    for_each = var.ingress_ipv6_cidr_blocks
+
+    content {
+      from_port = element(split(",", ingress.value), 0)
+      to_port = element(split(",", ingress.value), 1)
+      protocol = element(split(",", ingress.value), 2)
+      ipv6_cidr_blocks = [ element(split(",", ingress.value), 3) ]
+      description = element(split(",", ingress.value), 4)
+    }
+
+  }
+
+
+//  Ingress - Prefix List Ids
+  dynamic "ingress" {
+    for_each = var.ingress_prefix_list_ids
+
+    content {
+      from_port = element(split(",", ingress.value), 0)
+      to_port = element(split(",", ingress.value), 1)
+      protocol = element(split(",", ingress.value), 2)
+      prefix_list_ids = [ element(split(",", ingress.value), 3) ]
+      description = element(split(",", ingress.value), 4)
     }
 
   }
@@ -31,20 +74,47 @@ resource "aws_security_group" "security_group" {
 
   }
 
-//  Ingress - CIDR
-  dynamic "ingress" {
-    for_each = var.ingress_cidr_list
+//  Egress - CIDR IPv4
+  dynamic "egress" {
+    for_each = var.egress_cidr_list
 
     content {
-      from_port = element(split(",", ingress.value), 0)
-      to_port = element(split(",", ingress.value), 1)
-      protocol = element(split(",", ingress.value), 2)
-      cidr_blocks = [ element(split(",", ingress.value), 3) ]
-      description = element(split(",", ingress.value), 4)
+      from_port = element(split(",", egress.value), 0)
+      to_port = element(split(",", egress.value), 1)
+      protocol = element(split(",", egress.value), 2)
+      cidr_blocks = [ element(split(",", egress.value), 3) ]
+      description = element(split(",", egress.value), 4)
     }
 
   }
 
+//  Egress - CIDR IPv6
+  dynamic "egress" {
+    for_each = var.egress_ipv6_cidr_blocks
+
+    content {
+      from_port = element(split(",", egress.value), 0)
+      to_port = element(split(",", egress.value), 1)
+      protocol = element(split(",", egress.value), 2)
+      ipv6_cidr_blocks = [ element(split(",", egress.value), 3) ]
+      description = element(split(",", egress.value), 4)
+    }
+
+  }
+
+//  Egress - Prefix List Ids
+  dynamic "egress" {
+    for_each = var.egress_prefix_list_ids
+
+    content {
+      from_port = element(split(",", egress.value), 0)
+      to_port = element(split(",", egress.value), 1)
+      protocol = element(split(",", egress.value), 2)
+      prefix_list_ids = [ element(split(",", egress.value), 3) ]
+      description = element(split(",", egress.value), 4)
+    }
+
+  }
 
 //  Egress - Source SG
   dynamic "egress" {
@@ -55,20 +125,6 @@ resource "aws_security_group" "security_group" {
       to_port = element(split(",", egress.value), 1)
       protocol = element(split(",", egress.value), 2)
       security_groups = [ element(split(",", egress.value), 3) ]
-      description = element(split(",", egress.value), 4)
-    }
-
-  }
-
-//  Egress - CIDR
-  dynamic "egress" {
-    for_each = var.egress_cidr_list
-
-    content {
-      from_port = element(split(",", egress.value), 0)
-      to_port = element(split(",", egress.value), 1)
-      protocol = element(split(",", egress.value), 2)
-      cidr_blocks = [ element(split(",", egress.value), 3) ]
       description = element(split(",", egress.value), 4)
     }
 
--- aws/security-group/variables.tf
+++ aws/security-group/variables.tf
@@ -3,9 +3,29 @@ variable "vpc_id" {
   description = "The id of the VPC where the security group needs to be created"
 }
 
+# Ingress
+
+variable "ingress_cidr_list" {
+  type    = list(string)
+  description = "List of concatenated ports, protocols, CIDR (IPv4) blocks used for inbound rules"
+  default = []
+}
+
 variable "ingress_from_self_list" {
   type    = list(string)
   description = "List of concatenated ports, protocols from the same SG used for inbound rules"
+  default = []
+}
+
+variable "ingress_ipv6_cidr_blocks" {
+  type    = list(string)
+  description = "List of concatenated ports, protocols, CIDR (IPv6) blocks used for inbound rules"
+  default = []
+}
+
+variable "ingress_prefix_list_ids" {
+  type    = list(string)
+  description = "List of concatenated ports, protocols, prefix list ids used for inbound rules"
   default = []
 }
 
@@ -15,9 +35,23 @@ variable "ingress_source_sg_list" {
   default = []
 }
 
-variable "ingress_cidr_list" {
+# Egress
+
+variable "egress_cidr_list" {
   type    = list(string)
-  description = "List of concatenated ports, protocols, CIDR blocks used for inbound rules"
+  description = "List of concatenated ports, protocols, CIDR (IPv4) blocks used for outbound rules"
+  default = [ "0,0,-1,0.0.0.0/0" ]
+}
+
+variable "egress_ipv6_cidr_blocks" {
+  type    = list(string)
+  description = "List of concatenated ports, protocols, CIDR (IPv6) blocks used for outbound rules"
+  default = []
+}
+
+variable "egress_prefix_list_ids" {
+  type    = list(string)
+  description = "List of concatenated ports, protocols, prefix list ids used for outbound rules"
   default = []
 }
 
@@ -25,12 +59,6 @@ variable "egress_source_sg_list" {
   type    = list(string)
   description = "List of concatenated ports, protocols, source sgs used for outbound rules"
   default = []
-}
-
-variable "egress_cidr_list" {
-  type    = list(string)
-  description = "List of concatenated ports, protocols, CIDR blocks used for outbound rules"
-  default = [ "0,0,-1,0.0.0.0/0" ]
 }
 
 variable "stack_name" {
