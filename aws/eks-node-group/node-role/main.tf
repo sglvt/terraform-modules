94dc33995e24376e4b09acd0be41775fbf5ee2db
Serban Gilvitu <serbangilvitu@users.noreply.github.com>
2020-10-17 10:10:49 +0400

    aws/eks-node-group - Added Policy required for the cluster autoscaler

--- aws/eks-node-group/node-role/main.tf
+++ aws/eks-node-group/node-role/main.tf
@@ -32,3 +32,31 @@ resource "aws_iam_instance_profile" "node_profile" {
   name_prefix = var.cluster_name
   role = aws_iam_role.node_role.name
 }
+
+# Policy required for the cluster autoscaler
+# https://docs.aws.amazon.com/eks/latest/userguide/cluster-autoscaler.html
+resource "aws_iam_role_policy" "cluster_autoscaler" {
+  name = "cluster_autoscaler"
+  role = aws_iam_role.node_role.name
+
+  policy = <<-EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Action": [
+          "autoscaling:DescribeAutoScalingGroups",
+          "autoscaling:DescribeAutoScalingInstances",
+          "autoscaling:DescribeLaunchConfigurations",
+          "autoscaling:DescribeTags",
+          "autoscaling:SetDesiredCapacity",
+          "autoscaling:TerminateInstanceInAutoScalingGroup",
+          "ec2:DescribeLaunchTemplateVersions"
+        ],
+        "Effect": "Allow",
+        "Resource": "*"
+      }
+    ]
+  }
+  EOF
+}
